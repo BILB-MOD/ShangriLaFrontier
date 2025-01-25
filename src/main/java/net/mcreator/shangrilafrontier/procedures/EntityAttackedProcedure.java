db9ac98e35f8ac608700c46f501f67e84dec1afb
sinsamvae <anime@DESKTOP-VBE9402.lan>
2025-01-25 00:35:28 -0600

    sync

--- src/main/java/net/mcreator/shangrilafrontier/procedures/EntityAttackedProcedure.java
+++ src/main/java/net/mcreator/shangrilafrontier/procedures/EntityAttackedProcedure.java
@@ -5,7 +5,10 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 
+import net.minecraft.world.item.TridentItem;
+import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
@@ -39,7 +42,29 @@ public class EntityAttackedProcedure {
 		double damage = 0;
 		double stage = 0;
 		double crit = 0;
+		double sev = 0;
+		double tedz = 0;
+		double ted = 0;
+		double sevz2 = 0;
+		double sevy2 = 0;
+		double sevx2 = 0;
+		double cubesize = 0;
+		double sev2 = 0;
 		damage = Math.ceil((sourceentity.getCapability(ShangrilaFrontierModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new ShangrilaFrontierModVariables.PlayerVariables())).STR * 0.1);
+		for (int index0 = 0; index0 < 2; index0++) {
+			stage = stage + 1;
+			if (stage == 1) {
+				weapon = (sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY);
+			}
+			if (stage == 2) {
+				weapon = (sourceentity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY);
+			}
+			if (weapon.getItem() instanceof SwordItem || weapon.getItem() instanceof AxeItem || weapon.getItem() instanceof TridentItem) {
+				damage = damage + weapon.getDamageValue();
+				if (sourceentity instanceof Player _player && !_player.level().isClientSide())
+					_player.displayClientMessage(Component.literal((new java.text.DecimalFormat("DMG: ##").format(damage))), true);
+			}
+		}
 		crit = Mth.nextInt(RandomSource.create(), 1, 100);
 		if (crit <= (sourceentity.getCapability(ShangrilaFrontierModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new ShangrilaFrontierModVariables.PlayerVariables())).CritChance) {
 			damage = Math.ceil(damage + ((sourceentity.getCapability(ShangrilaFrontierModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new ShangrilaFrontierModVariables.PlayerVariables())).CritDmg * damage) / 100);
